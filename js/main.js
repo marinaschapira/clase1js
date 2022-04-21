5432f3d7ce55bb5f645cd1f2d6b09d0f4ab787a0
MarinaSchapira <mschapira@hotmail.com>
2022-04-21 09:03:34 -0300

    prueba1

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/*console.log("hola desde js");*/
+
+alert("Hello World")
+document.write ("<h1>Hello World</h1>")
+document.write(3+5)
+nom=prompt("ingrese su nombre")
+document.write("Hola ",nom)
+ 
+var num1=prompt("ingrese numero 1")
+
+var num2=prompt("ingrese numero2")
+
+num1=parseInt(num1)
+num2=parseInt(num2)
+document.write(num1+num2)
+
+ 
